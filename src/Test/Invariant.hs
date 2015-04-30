6a22da94d8ccaef50948ad45c266180cdc56290d
knupfer <fknupfer@gmail.com>
2015-04-30 12:30:49 +0200

    define infix function in infix

--- src/Test/Invariant.hs
+++ src/Test/Invariant.hs
@@ -27,7 +27,7 @@ involutory :: Eq a => (a -> a) -> a -> Bool
 involutory f x = f (f x) == x
 
 inverts :: Eq a => (b -> a) -> (a -> b) -> a -> Bool
-inverts f g x = f (g x) == x
+(f `inverts` g) x = f (g x) == x
 
 commutative :: Eq b => (a -> a -> b) -> a -> a -> Bool
 commutative f x y = x `f` y == y `f` x
@@ -36,13 +36,13 @@ associative :: Eq a => (a -> a -> a) -> a -> a -> a -> Bool
 associative f x y z = x `f` (y `f` z) == (x `f` y) `f` z
 
 distributesLeftOver :: Eq a => (a -> a -> a) -> (a -> a -> a) -> a -> a -> a -> Bool
-distributesLeftOver f g x y z = x `f` (y `g` z) == (x `f` y) `g` (x `f` z)
+(f `distributesLeftOver` g) x y z = x `f` (y `g` z) == (x `f` y) `g` (x `f` z)
 
 distributesRightOver :: Eq a => (a -> a -> a) -> (a -> a -> a) -> a -> a -> a -> Bool
-distributesRightOver f g x y z = (y `g` z) `f` x == (x `f` y) `g` (x `f` z)
+(f `distributesRightOver` g) x y z = (y `g` z) `f` x == (x `f` y) `g` (x `f` z)
 
 distributesOver :: Eq a => (a -> a -> a) -> (a -> a -> a) -> a -> a -> a -> Bool
-distributesOver f g x y z = (f `distributesLeftOver` g) x y z
+(f `distributesOver` g) x y z = (f `distributesLeftOver` g) x y z
                             && (f `distributesRightOver` g) x y z
 
 inflating :: (Foldable f, Foldable f') => (f a -> f' b) -> f a -> Bool
@@ -52,7 +52,7 @@ deflating :: (Foldable f, Foldable f') => (f a -> f' b) -> f a -> Bool
 deflating f xs = null xs || length (f xs) < length xs
 
 cyclesWithin :: Eq a => (a -> a) -> Int -> a -> Bool
-cyclesWithin f n x = go [] . take (n + 1) $ iterate f x
+(f `cyclesWithin` n) x = go [] . take (n + 1) $ iterate f x
              where go xs (y:ys) | y `elem` xs = True
                                 | otherwise   = go (y:xs) ys
                    go _ _ = False
@@ -64,4 +64,4 @@ translationInvariant :: (Num a, Eq b) => (a -> a -> b) -> a -> a -> a -> Bool
 translationInvariant f = f `invariatesOver` (+)
 
 invariatesOver :: Eq b => (a -> a -> b) -> (a -> c -> a) -> a -> a -> c -> Bool
-invariatesOver f g x y z = x `f` y == (x `g` z) `f` (y `g` z)
+(f `invariatesOver` g) x y z = x `f` y == (x `g` z) `f` (y `g` z)
